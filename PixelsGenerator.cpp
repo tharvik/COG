b5cac9a17170ad503faf87ba0a47594acbcd5668
Kéwan Marconnet <tharvik@gmail.com>
2014-01-24 16:10:29 +0100

    Remove sign warning

--- PixelsGenerator.cpp
+++ PixelsGenerator.cpp
@@ -11,10 +11,10 @@ template <typename T>
 T* generateRandomPixels(GLushort width, GLushort height, GLubyte depth)
 {
 	srand((int) time(NULL));
-	
+
 	T* pixels = (T*) calloc(width * height * depth, sizeof(T));
-	for (GLuint i = 0; i < width * height * depth; i++)
+	for (GLuint i = 0; i < (GLuint) width * height * depth; i++)
 		pixels[i] = rand() % (T) (ceil(pow(2, 8 * sizeof(T))) - 1);
-		
+
 	return pixels;
 }
