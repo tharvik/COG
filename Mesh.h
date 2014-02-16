2e41320940aa630837ddedc3dc0e8e3f97dc2784
VianneyRousset <Vianney@vianney.home>
2014-02-16 12:12:17 +0100

    Revert 2cdf0e3..48ba81e
    This rolls back to commit 2cdf0e36870888ae7b8149397297628df8f47fb3.

--- Mesh.h
+++ Mesh.h
@@ -11,7 +11,7 @@ private:
         std::vector<std::array<float, 3>> v;
         std::vector<std::array<float, 2>> vt;
         std::vector<std::array<std::array<unsigned short, 2>, 3>> f;
-
+        
 public:
         // Constructors
         Mesh();
@@ -20,9 +20,9 @@ public:
              std::vector<std::array<float, 2>> vt,
              std::vector<std::array<std::array<unsigned short, 2>, 3>>
              f);
-
+        
         void draw();
-
+        
         bool operator<(const Mesh &b) const;
         
         // Destructor
