f8911c64065351729eab3dc229a7bc96373f8cde
Kéwan Marconnet <tharvik@gmail.com>
2014-02-16 12:59:18 +0100

    Remove MeshVertice, unneeded

--- Mesh.cpp
+++ Mesh.cpp
@@ -9,28 +9,23 @@ Mesh::Mesh(std::vector<std::array<float, 3>> v,
 		std::vector<unsigned short> indices)
 		: sizeIndices(indices.size())
 {
-	std::vector<MeshVertice> pts;
-
-	auto j = vt.begin();
-	for(auto i = v.begin(); i != v.end() && j != vt.end(); i++, j++) {
-		
-#ifdef __APPLE__
-		MeshVertice a = {*i, *j};
-		pts.push_back(a);
-#else
-		pts.push_back( { *i, *j } );
-#endif
-	}
-
 	glGenBuffers(3, this->buffers.data());
 
 	// copy vertices
 	glBindBuffer(GL_ARRAY_BUFFER, this->buffers[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(pts[0]) * pts.size(), pts.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(v[0]) * v.size(), v.data(),
+			GL_STATIC_DRAW);
+
+	// copy vertices texture
+	glBindBuffer(GL_ARRAY_BUFFER, this->buffers[2]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vt[0]) * vt.size(), vt.data(),
+			GL_STATIC_DRAW);
 
 	// copy indices
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->buffers[0]);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices[0]) * indices.size(), indices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER,
+			sizeof(indices[0]) * indices.size(),
+			indices.data(), GL_STATIC_DRAW);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -38,7 +33,15 @@ Mesh::Mesh(std::vector<std::array<float, 3>> v,
 
 bool Mesh::operator<(const Mesh &b) const
 {
-	return (this->buffers[0] + this->buffers[1]) < (b.buffers[0] + b.buffers[1]);
+	unsigned short x = 0, y = 0;
+
+	for(auto i : this->buffers)
+		x += i;
+
+	for(auto i : b.buffers)
+		y += i;
+
+	return x < y;
 }
 
 void Mesh::draw()
@@ -48,8 +51,9 @@ void Mesh::draw()
 
 	// setup vertices
 	glBindBuffer(GL_ARRAY_BUFFER, this->buffers[1]);
-	glVertexPointer(3, GL_FLOAT, sizeof(MeshVertice), 0);
-	glTexCoordPointer(2, GL_FLOAT, sizeof(MeshVertice), (char*) NULL + offsetof(MeshVertice,vt));
+	glVertexPointer(3, GL_FLOAT, 0, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, this->buffers[2]);
+	glTexCoordPointer(2, GL_FLOAT, 0, 0);
 
 	// draw elements
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, this->buffers[0]);
@@ -57,7 +61,7 @@ void Mesh::draw()
 
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	
+
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 }
