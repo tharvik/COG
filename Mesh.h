d0e833120de1ecf1baae9180b6fa271bdf5825f6
Kéwan Marconnet <tharvik@gmail.com>
2014-02-17 14:01:53 +0100

    Add documentation to Mesh and MeshManager

--- Mesh.h
+++ Mesh.h
@@ -5,28 +5,85 @@
 #include <string>
 #include <vector>
 
+/**
+ * Represent a set of verticles and faces to draw
+ *
+ * Used by Object which handle Texture and Shader
+ */
 class Mesh {
 	private:
+		/**
+		 * Number of indices to draw
+		 */
 		unsigned int sizeIndices;
+
+		/**
+		 * Buffers used by OpenGL
+		 *
+		 * buffers[0] contains the indices\n
+		 * buffers[1] contains the vertices\n
+		 * buffers[2] contains the vertices on the texture\n
+		 * buffers[3] contains the normals\n
+		 */
 		std::array<GLuint,4> buffers;
 
 	public:
-		// Constructors
+		/**
+		 * Default constructor
+		 */
 		Mesh();
+
+		/**
+		 * Move constructor
+		 *
+		 * Leave m in a valid but useless state
+		 *
+		 * \param m Mesh to move, which will be left in the default
+		 * state
+		 */
 		Mesh(Mesh&& m);
 
-		// v: verticle,
-		// vt: verticle texture,
-		// indices starting from 0
+		/**
+		 * Construct with the given vertices, vertices on the texture
+		 * and indices
+		 *
+		 * \deprecated
+		 * By moving to .mesh file in MeshManager, we are leaving
+		 * this uneeded constructor
+		 *
+		 * \param v Vertices
+		 * \param vt Vertices on the texture
+		 * \param indices Indices to write
+		 */
 		Mesh(std::vector<std::array<float, 3>> v,
 				std::vector<std::array<float, 2>> vt,
 				std::vector<unsigned short> indices);
 
+		/**
+		 * Construct with the given buffers and number of indices
+		 *
+		 * \param buffers Buffers initialized and filled as \ref buffers
+		 *
+		 * \param sizeIndices Number of indices to draw
+		 */
 		Mesh(std::array<GLuint,4> buffers, unsigned int sizeIndices);
 
+		/**
+		 * Draw itself
+		 *
+		 * Bettre use it after binding the texture
+		 */
 		void draw();
 
-		bool operator<(const Mesh &b) const;
+		/**
+		 * Implement the less operator based on the values of the
+		 * buffers
+		 *
+		 * \param m Other mesh to compare to
+		 *
+		 * \return Return true if \f$ \sum_{i=0}^{4} buffers_i.size() < \sum_{i=0}^{4} b.buffers_i.size()\f$
+		 */
+		bool operator<(const Mesh &m) const;
 
 		// Destructor
 		~Mesh();
--- MeshManager.h
+++ MeshManager.h
@@ -9,32 +9,92 @@
 #include "Logger.h"
 #include "Mesh.h"
 
+/**
+ * Manage Mesh, avoid reloading a Mesh if already done
+ */
 class MeshManager {
-private:
-        void parse();
-        
-        template<unsigned short size>
-        std::array<float, size> parseV();
-        std::array<std::array<unsigned short, 2>, 3> parseF();
-        
-        void cleanLine(const std::string word, const bool log);
-        
-        std::map<std::string,Mesh> map;
-        
-        // current loading
-        std::string path;
-        std::ifstream file;
-        unsigned short lineCount;
-        
-public:
-        // Constructor
-        MeshManager();
-        
-	// load a .mesh file, still a draft, will remplace .obj
-        Mesh& loadMesh(const std::string path);
+	private:
+		/**
+		 * Parse a vertice
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 *
+		 * \return Parsed vertice, of the given size
+		 */
+		template<unsigned short size>
+			std::array<float, size> parseV();
 
-        Mesh& load(const std::string path);
-        
-        // Destructor
-        ~MeshManager();
+		/**
+		 * Parse a face
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 *
+		 * \return Parsed face
+		 */
+		std::array<std::array<unsigned short, 2>, 3> parseF();
+
+		/**
+		 * Discard the remaining line
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 *
+		 * \param word The parsing word
+		 * \param log True if we have to logger the remaining line
+		 */
+		void cleanLine(const std::string word, const bool log);
+
+		/**
+		 * Map of already loaded file
+		 */
+		std::map<std::string,Mesh> map;
+
+		/**
+		 * Last loaded path
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 */
+		std::string path;
+
+		/**
+		 * Current loaded file, is valid during execution of \ref load
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 */
+		std::ifstream file;
+
+		/**
+		 * Current parsing line, give an error line
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 */
+		unsigned short lineCount;
+
+	public:
+		/**
+		 * Load a .mesh file, temporary and draft function to avoid
+		 * breaking existing code
+		 *
+		 * \deprecated After moving to .mesh file, we won't need this
+		 * anymore
+		 *
+		 * \param path to load the .mesh from
+		 *
+		 * \return A reference to the wanted Mesh
+		 */
+		Mesh& loadMesh(const std::string path);
+
+		/**
+		 * Load the file at the given path
+		 *
+		 * \param path to load the .obj from
+		 *
+		 * \return A reference to the wanted Mesh
+		 */
+		Mesh& load(const std::string path);
 };
