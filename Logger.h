52d78ec76bee8b8065b58ccf176fffcf6c587307
tharvik <tharvik@gmail.com>
2014-08-19 10:02:19 +0200

    Use a #defined minimum level with the Logger

--- Logger.h
+++ Logger.h
@@ -1,7 +1,11 @@
 #include <iostream>
+#include "config.h"
 
-#define logger_info(msg)       logger_log("INFO : ", msg)
-#define logger_warn(msg)       logger_log("WARN : ", msg)
-#define logger_error(msg)      logger_log("ERROR: ", msg)
-#define logger_log(type, msg)  std::cerr << __FILE__ ":" << __LINE__	\
-				<< ": " type << ": " << msg << std::endl
+#define logger_info(msg)       logger_log(0, "INFO : ", msg)
+#define logger_warn(msg)       logger_log(1, "WARN : ", msg)
+#define logger_error(msg)      logger_log(2, "ERROR: ", msg)
+#define logger_log(level, type, msg)					\
+	if(level >= LOGGER_LEVEL) {					\
+	std::cerr << __FILE__ ":" << __LINE__ << ": " type << ": "	\
+		  << msg << std::endl;					\
+	}
