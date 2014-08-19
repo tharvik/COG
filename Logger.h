199cf442f5b3bb4c2bf36775e0bd46f3abfe91f7
tharvik <tharvik@gmail.com>
2014-08-19 15:47:38 +0200

    Improve Logger, update suppressions.valgrind

--- Logger.h
+++ Logger.h
@@ -1,11 +1,11 @@
 #include <iostream>
 #include "config.h"
 
-#define logger_info(msg)       logger_log(0, "INFO : ", msg)
-#define logger_warn(msg)       logger_log(1, "WARN : ", msg)
-#define logger_error(msg)      logger_log(2, "ERROR: ", msg)
+#define logger_info(msg)       logger_log(0, "INFO ", msg)
+#define logger_warn(msg)       logger_log(1, "WARN ", msg)
+#define logger_error(msg)      logger_log(2, "ERROR", msg)
 #define logger_log(level, type, msg)					\
 	if(level >= LOGGER_LEVEL) {					\
-	std::cerr << __FILE__ ":" << __LINE__ << ": " type << ": "	\
-		  << msg << std::endl;					\
+	std::cerr << __FILE__ ":" << __LINE__ << ":" << __func__	\
+		<< ": " type << ": " << msg << std::endl;		\
 	}
